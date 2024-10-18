08c5022288816ad0d1ecd73f5a55b1021c5ca882
OFrancisco3 <leandrofrava@outlook.com>
2024-10-18 00:11:21 -0500

    commit3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"http://leandrofrancisco03.github.io/FIEI_biblioteca"
+  base:"https://leandrofrancisco03.github.io/FIEI_biblioteca"
 });
